179638bf996e90e18773892df4b6348379613df8
Tuomas Suutari <tuomas@nepnep.net>
2022-05-30 11:17:48 +0300

    Lisää testi: 404 / Tehtävää ei löydy

--- tehtavat/tests/test_views.py
+++ tehtavat/tests/test_views.py
@@ -27,4 +27,14 @@ def test_tehtava_sivu(client):
     assert vastaus.status_code == 200
     html_sisalto = vastaus.content.decode("utf-8")
     assert "<title>Tehtävä: Aja testit</title>" in html_sisalto
- 
+
+
+@pytest.mark.django_db
+def test_tehtava_sivu_ei_loydy(client):
+    url = reverse('tehtava', kwargs={'id': 0})
+
+    vastaus = client.get(url)
+
+    assert isinstance(vastaus, HttpResponse)
+    assert vastaus.status_code == 404
+    assert "Tehtävää 0 ei löydy." in vastaus.content.decode("utf-8")
